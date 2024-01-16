5c7c4b1b5864b9031572b11152b24a6812d03680
Abhinandan-Sah <abhinandansah222@gmail.com>
2024-01-16 10:52:56 +0530

    updated

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+      System.out.println("Hello World");
+      System.out.println("java is purely object oriented programming language");
+    }
+  }
+
+  
+  
